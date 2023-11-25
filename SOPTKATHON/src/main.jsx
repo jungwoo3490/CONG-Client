7bf60fc9a5b634521d3e809f9efff2d73b6107b1
ljh0608 <m98m@catholic.ac.kr>
2023-11-26 00:07:12 +0900

    style: globalStyle적용

--- SOPTKATHON/src/main.jsx
+++ SOPTKATHON/src/main.jsx
@@ -1,9 +1,14 @@
 import App from './App.jsx';
+import GlobalStyle from './styles/globalStyle.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ SOPTKATHON/src/styles/theme.js
@@ -0,0 +1,23 @@
+const colors = {
+  mainGreen: '#9FC743',
+  mainLightGrey: '#F8FAED',
+  mainBlack: '#121212',
+  mainWhite: '#FFFFFF',
+  mainGrey: '#E9E9E9',
+  fontGreen: '#9FC743',
+  fontGrey: '#878787',
+  errorRed: '#FF0000',
+  backgroundGrey: '#f2f2f2',
+};
+
+const fontSize = {
+  md: '1rem', // 16
+  lg: '1.5rem',
+  xl: '1.75rem',
+  xxl: '4rem',
+};
+
+export const theme = {
+  colors,
+  fontSize,
+};
